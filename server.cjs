cb7be66b06aa2ce315698935973b9fea9163d99d
mamasexeno <105907107+mamasexeno@users.noreply.github.com>
2024-10-29 18:46:52 +0700

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -4,14 +4,12 @@ const PORT = 3000;
 
 const formatsRouter = require('./src/routes/formats');
 const downloadRouter = require('./src/routes/download');
-const downloadSubtitleRouter = require('./src/routes/downloadSubtitle');
 const thumbnailRouter = require('./src/routes/thumbnail');
 
 //use route
 app.use(express.json());
 app.use('/formats', formatsRouter);
 app.use('/download', downloadRouter);
-app.use('/downloadSubtitle', downloadSubtitleRouter);
 app.use('/thumbnail', thumbnailRouter);
 
 app.listen(PORT, () => {
