db26364743d695dafaaef997e8d0771ea0adfd37
Rafa Arfiansyah Raif <rfa.ar2005@gmail.com>
2024-10-29 12:44:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Import rute
 const formatsRouter = require('./routes/formats');
 const downloadRouter = require('./routes/download');
 const downloadSubtitleRouter = require('./routes/downloadSubtitle');
 const thumbnailRouter = require('./routes/thumbnail');
 
-// Middleware untuk parsing JSON
+//use route
 app.use(express.json());
-
-// Gunakan rute
 app.use('/formats', formatsRouter);
 app.use('/download', downloadRouter);
 app.use('/downloadSubtitle', downloadSubtitleRouter);
